8dc33b5c2863a5700600f5f3f7546bda05015c0e
A00023488 Vanessa Aigbekaen <A00023488@myTUDublin.ie>
2026-03-06 10:36:22 +0000

    Update the src files

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,121 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<head>
+        <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Multi-Product Card</title>
+
+        <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet">
+    
+        <!-- Link to Bootstrap Stylesheet-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+
+        <!-- Link to My Stylesheet-->
+        <link rel="stylesheet" href="src/public/css/style.css">
+</head>
+<body>
+    <div class="container">
+        <div class="top-section">
+            <h1 class="header">Sneaker For Sale 10%</h1>
+        </div>
+        <div class="product-card">
+            <div class="product-image">
+                <img src="src/public/productImage/product1.webp" alt="Pink Shoes">
+            </div>
+            <div class="product-info">
+                <h2 class="product-title">Product Title</h2>
+                <p class="product-desc">Product description and details</p>
+                <h3 class="product-price">$448.50</h3>
+                <button class="cart">Add To Cart</button>
+            </div>
+        </div>
+        <div class="product-card">
+            <div class="product-image">
+                <img src="src/public/productImage/product2.webp" alt="Pink Shoes">
+            </div>
+            <div class="product-info">
+                <h2 class="product-title">Product Title</h2>
+                <p class="product-desc">Product description and details</p>
+                <h3 class="product-price">$508.50</h3>
+                <button class="cart">Add To Cart</button>
+            </div>
+        </div>
+        <div class="product-card">
+            <div class="product-image">
+                <img src="src/public/productImage/product3.webp" alt="Pink Shoes">
+            </div>
+            <div class="product-info">
+                <h2 class="product-title">Product Title</h2>
+                <p class="product-desc">Product description and details</p>
+                <h3 class="product-price">$350.99</h3>
+                <button class="cart">Add To Cart</button>
+            </div>
+        </div>
+            <div class="product-card">
+            <div class="product-image">
+                <img src="src/public/productImage/product4.webp" alt="Pink Shoes">
+            </div>
+            <div class="product-info">
+                <h2 class="product-title">Product Title</h2>
+                <p class="product-desc">Product description and details</p>
+                <h3 class="product-price">$448.50</h3>
+                <button class="cart">Add To Cart</button>
+            </div>
+        </div>
+        <div class="product-card">
+            <div class="product-image">
+                <img src="src/public/productImage/product5.webp" alt="Pink Shoes">
+            </div>
+            <div class="product-info">
+                <h2 class="product-title">Product Title</h2>
+                <p class="product-desc">Product description and details</p>
+                <h3 class="product-price">$508.50</h3>
+                <button class="cart">Add To Cart</button>
+            </div>
+        </div>
+        <div class="product-card">
+            <div class="product-image">
+                <img src="src/public/productImage/product6.webp" alt="Pink Shoes">
+            </div>
+            <div class="product-info">
+                <h2 class="product-title">Product Title</h2>
+                <p class="product-desc">Product description and details</p>
+                <h3 class="product-price">$350.99</h3>
+                <button class="cart">Add To Cart</button>
+            </div>
+        </div>
+            <div class="product-card">
+            <div class="product-image">
+                <img src="src/public/productImage/product7.jpg" alt="Pink Shoes">
+            </div>
+            <div class="product-info">
+                <h2 class="product-title">Product Title</h2>
+                <p class="product-desc">Product description and details</p>
+                <h3 class="product-price">$448.50</h3>
+                <button class="cart">Add To Cart</button>
+            </div>
+        </div>
+        <div class="product-card">
+            <div class="product-image">
+                <img src="src/public/productImage/product8.jpg" alt="Pink Shoes">
+            </div>
+            <div class="product-info">
+                <h2 class="product-title">Product Title</h2>
+                <p class="product-desc">Product description and details</p>
+                <h3 class="product-price">$508.50</h3>
+                <button class="cart">Add To Cart</button>
+            </div>
+        </div>
+        <div class="product-card">
+            <div class="product-image">
+                <img src="src/public/productImage/product9.jfif" alt="Pink Shoes">
+            </div>
+            <div class="product-info">
+                <h2 class="product-title">Product Title</h2>
+                <p class="product-desc">Product description and details</p>
+                <h3 class="product-price">$350.99</h3>
+                <button class="cart">Add To Cart</button>
+            </div>
+        </div>
+    </div>
+</body>
